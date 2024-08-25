a2801fd83bd872fdf14f1ed637088af12f3040c2
Sairam kola <sai@plex.engineer>
2024-08-25 12:10:59 +0530

    update url

--- scripts/dydx.js
+++ scripts/dydx.js
@@ -2,11 +2,10 @@ const { db } = require("./db");
 const axios = require('axios');
 const fetchAndStoreDydxData = async (blockTimestamp) => {
     try {
-        const time = new Date(blockTimestamp * 1000).toISOString();
-        const ethResponse = await axios.get(`https://indexer.dydx.trade/v4/historicalFunding/ETH-USD?effectiveBeforeOrAt=${time}&limit=1`);
-        const btcResponse = await axios.get(`https://indexer.dydx.trade/v4/historicalFunding/BTC-USD?effectiveBeforeOrAt=${time}&limit=1`);
-        const ethFundingRate = ethResponse.data.historicalFunding[0].rate;
-        const btcFundingRate = btcResponse.data.historicalFunding[0].rate;
+        const ethResponse = await axios.get("https://indexer.dydx.trade/v4/perpetualMarkets?ticker=ETH-USD");
+        const btcResponse = await axios.get("https://indexer.dydx.trade/v4/perpetualMarkets?ticker=BTC-USD");
+        const ethFundingRate = ethResponse.data.markets["ETH-USD"].nextFundingRate;
+        const btcFundingRate = btcResponse.data.markets["BTC-USD"].nextFundingRate;
         await db.promise("INSERT INTO dydx(eth_funding_rate, btc_funding_rate, timestamp, date) VALUES ($1,$2,$3,$4)", [Number(ethFundingRate).toFixed(10), Number(btcFundingRate).toFixed(10), blockTimestamp, new Date(blockTimestamp * 1000)]);
     }
     catch (err) {
